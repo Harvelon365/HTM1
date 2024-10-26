67d854bed31e4078ee2ab213eae597b8aa626931
Harvey Tucker <harvelon@outlook.com>
2024-10-26 13:29:59 +0100

    fix indentation in parser

--- parse.py
+++ parse.py
@@ -4,25 +4,25 @@ import htm1.py
 stack = []
 
 class HTMLParser(HTMLParser):
-    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]):
-        i = -1
-        while True:
-            i += 1
-            op = ()
-            if attrs[i][0].lower() == 'id':
-                # get op from id length
-            else:
-                # get op from tag length
-                continue
-            match op[1]:
-                    case 0:
-                        continue
-                    case 1:
-                        i += 1
-                        stack.append(len(attrs[i][1]))
+	def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]):
+		i = -1
+		while True:
+			i += 1
+			op = ()
+			if attrs[i][0].lower() == 'id':
+				# get op from id length
+			else:
+				# get op from tag length
+				continue
+			match op[1]:
+					case 0:
+						continue
+					case 1:
+						i += 1
+						stack.append(len(attrs[i][1]))
 
 
 def parseHTML(html):
-    parser = HTMLParser()
-    parser.feed(html)
-    return stack
+	parser = HTMLParser()
+	parser.feed(html)
+	return stack
