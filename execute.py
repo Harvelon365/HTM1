014a1b9cddf16dddf56119ed1274958c68f7f15d
jacob <jacobhalleron@pm.me>
2024-10-26 13:11:58 +0100

    annotate params for commands

---
+++ execute.py
@@ -0,0 +1,6 @@
+test_code = [
+
+]
+
+def exe_htm1(htm1_code):
+	for i in htm1_code:
---
+++ htm1.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+commands = [
+	("null", 0),	# ignore
+	("push", 2),	# x = stack, y  = data
+	("pop", 2),		# x = stack from, y = stack to
+	("op", 2),		# x = stack, y = opcode
+	("break", 0),	#
+	("input", 1),	# x = stack
+	("output", 1),	# x = stack
+	("if", 1),		# x = stack
+	("loop", 0),	#
+	("flip", 1),	# x = stack
+]
+
+operations = [
+	"+",
+	"-",
+	"*",
+	"/",
+	"delete",
+	"duplicate",
+]
