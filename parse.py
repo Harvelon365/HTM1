ef74ac4ed329ece63688e118bb110394c6b7b9fd
Harvey Tucker <harvelon@outlook.com>
2024-10-26 13:17:13 +0100

    fix merge issues

---
+++ parse.py
@@ -0,0 +1,28 @@
+from html.parser import HTMLParser
+import htm1.py
+
+stack = []
+
+class HTMLParser(HTMLParser):
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]):
+        i = -1
+        while True:
+            i += 1
+            op = ()
+            if attrs[i][0].lower() == 'id':
+                # get op from id length
+            else:
+                # get op from tag length
+                continue
+            match op[1]:
+                    case 0:
+                        continue
+                    case 1:
+                        i += 1
+                        stack.append(len(attrs[i][1]))
+
+
+def parseHTML(html):
+    parser = HTMLParser()
+    parser.feed(html)
+    return stack
